530fa3afc1fcb8458d3c2079fca9155be66db0ce
Sri Sethumeena S <meenapillai2006@gmail.com>
2026-01-04 18:39:52 +0530

    Add server index file

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import userroutes from "./routes/auth.js";
+import questionroute from "./routes/question.js";
+import answerroutes from "./routes/answer.js";
+import postRoutes from "./routes/post.js";
+import friendRoutes from "./routes/friend.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.use("/user", userroutes);
+app.use("/question", questionroute);
+app.use("/answer", answerroutes);
+app.use("/post", postRoutes);
+app.use("/friend", friendRoutes);
+
+const PORT = process.env.PORT || 5000;
+const databaseurl = process.env.MONGODB_URL;
+
+mongoose
+  .connect(databaseurl)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
