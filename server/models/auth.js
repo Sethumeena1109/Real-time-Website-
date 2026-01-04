608754c4218b745a5db9017fa65d64b39b96418a
Sri Sethumeena S <meenapillai2006@gmail.com>
2026-01-04 13:09:43 +0530

    Add auth model

---
+++ server/models/auth.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  about: {
+    type: String
+  },
+  tags: {
+    type: [String]
+  },
+  joinDate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model("user", userSchema);
