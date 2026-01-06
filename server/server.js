2f5d922e065df199db34d2790541c7d23c3ae713
Sri Sethumeena S <meenapillai2006@gmail.com>
2026-01-06 22:48:31 +0530

    Initialize Express server with routes and MongoDB

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js";
+import friendRoutes from "./routes/friend.js";
+import paymentRoutes from "./routes/payment.js";
+import postRoutes from "./routes/post.js";
+import questionRoutes from "./routes/question.js";
+import rewardRoutes from "./routes/reward.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error(err));
+
+// Register routes
+app.use("/auth", authRoutes);
+app.use("/friends", friendRoutes);
+app.use("/payment", paymentRoutes);
+app.use("/posts", postRoutes);
+app.use("/questions", questionRoutes);
+app.use("/rewards", rewardRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
