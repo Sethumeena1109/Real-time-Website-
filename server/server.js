9649d9a8071a3b59849cf1749e74430079a0d0e2
Sri Sethumeena S <meenapillai2006@gmail.com>
2026-01-06 23:00:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import otpRoutes from "./routes/otp.js";
 import authRoutes from "./routes/auth.js";
 import friendRoutes from "./routes/friend.js";
 import paymentRoutes from "./routes/payment.js";
 import postRoutes from "./routes/post.js";
 import questionRoutes from "./routes/question.js";
 import rewardRoutes from "./routes/reward.js";
+import otpRoutes from "./routes/otp.js"; // Added OTP routes
 
 dotenv.config();
 
@@ -16,13 +16,14 @@ const app = express();
 
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error(err));
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 // Register routes
 app.use("/auth", authRoutes);
@@ -31,6 +32,7 @@ app.use("/payment", paymentRoutes);
 app.use("/posts", postRoutes);
 app.use("/questions", questionRoutes);
 app.use("/rewards", rewardRoutes);
+app.use("/otp", otpRoutes); // OTP route registration
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
